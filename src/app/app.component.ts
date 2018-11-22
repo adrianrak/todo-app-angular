16e6625807aeb4fe81dc27d4af77ccaef539cdeb
Adrian Rak <adrrak@tlen.pl>
2018-11-22 18:25:49 +0100

    add task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,8 @@ export class AppComponent {
   tasksList: Array<string> = [];
   tasksDoneList: Array<string> = [];
 
-  add() {
-    this.tasksList.push(this.newTask);
-    this.newTask = '';
-    console.log(this.tasksList);
+  add(task: string) {
+    this.tasksList.push(task);
   }
 
   remove(task: string) {

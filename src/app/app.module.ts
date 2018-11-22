938236add23a8a1342ecd362ce0a379f38e5fa3c
Adrian Rak <adrrak@tlen.pl>
2018-11-22 18:41:19 +0100

    add todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AddTaskComponent } from './add-task/add-task.component';
+import { TodoTaskComponent } from './todo-task/todo-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    TodoTaskComponent
   ],
   imports: [
     BrowserModule,

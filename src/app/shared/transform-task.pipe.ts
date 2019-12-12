12ce205df513ee1c27d4a7b4569bff4beef00642
Adrian Rak <adrian.rak@empirica.pl>
2019-12-12 18:24:06 +0100

    update dependencies

--- src/app/shared/transform-task.pipe.ts
+++ src/app/shared/transform-task.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TransformTaskPipe implements PipeTransform {
 
-  transform(value: string, args?: string = ''): any {
+  transform(value: string, args: string = ''): any {
     return value.charAt(0).toUpperCase() + value.slice(1) + args;
   }
 
